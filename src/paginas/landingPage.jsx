afab3352819dc90a36293b4494d1fa5c78996e76
Eduardo <joselincango73@gmail.com>
2025-01-12 20:05:02 -0500

    Inicio del desarrollo Frontend

--- src/paginas/landingPage.jsx
+++ src/paginas/landingPage.jsx
@@ -1,27 +1,54 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '/src/Estilos/landinPage.css';
-import logo1 from '../assets/code.png'
-
+import '../Estilos/landinPage.css';
+import logoDarkMode from '../assets/dark.png';
+import logoFacebook from '../assets/facebook.png';
+import logoGithub from '../assets/github.png';
+import logoLinkedind from '../assets/linkedin.png';
+import logoRocket from '../assets/rocket.webp';
+import logoCode from '../assets/code.png';
+import logoConsulting from '../assets/consulting.png';
+import logoDesign from '../assets/design.png';
+import logoWeb1 from '../assets/web1.png';
+import logoWeb2 from '../assets/web2.png';
+import logoWeb3 from '../assets/web3.png';
+import logoWeb4 from '../assets/web4.png';
+import logoWeb5 from '../assets/web5.png';
+import logoWeb6 from '../assets/web6.png';
 
 export const LandingPage = () => {
   const [darkMode, setdarkMode] = useState(false)
   return (
     <div className={darkMode ? "dark" : ""}>
 
-      <main class="">
+      <main>
         <section>
-          <nav class="">
-            <h1 class="">TiendAnimal</h1>
-            <ul class="">
-              <li><img onclick="" /></li>
-              <img className='logo1' src={logo1} alt="Logo" />
+          <nav class="barra1">
+            <h1 class="title1">TiendAnimal</h1>
+            <ul>
+              <li>Conocenos</li>
+              <li>Planes alimenticos</li>
+              <li>Contactanos</li>
+              <li>
+                <img
+                  onClick={() => setdarkMode(!darkMode)}
+                  className="cursor-pointer"
+                  src={logoDarkMode}
+                  alt="logo"
+                />
+              </li>
               <li><a href="">Login</a></li>
             </ul>
           </nav>
         </section>
-      </main>
 
+        <section>
+          <p>Esta apgina esta dedicada a integrar un concepto de como los dueños <br/>
+          deben de relacionar la salud de sus mascotas para la mejora del bienestar de <b/>
+          las mismas.
+          </p>
+        </section>
+      </main>
     </div>
   )
 }
