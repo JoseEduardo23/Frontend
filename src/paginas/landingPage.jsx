cd038f8e7b2a0f5c1986b69ef95e9d29d7891c19
Eduardo <joselincango73@gmail.com>
2025-01-13 22:32:31 -0500

    Realizacion de la landinpage

--- src/paginas/landingPage.jsx
+++ src/paginas/landingPage.jsx
@@ -2,19 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../Estilos/landinPage.css';
 import logoDarkMode from '../assets/dark.png';
-import logoFacebook from '../assets/facebook.png';
-import logoGithub from '../assets/github.png';
-import logoLinkedind from '../assets/linkedin.png';
-import logoRocket from '../assets/rocket.webp';
-import logoCode from '../assets/code.png';
-import logoConsulting from '../assets/consulting.png';
-import logoDesign from '../assets/design.png';
-import logoWeb1 from '../assets/web1.png';
-import logoWeb2 from '../assets/web2.png';
-import logoWeb3 from '../assets/web3.png';
-import logoWeb4 from '../assets/web4.png';
-import logoWeb5 from '../assets/web5.png';
-import logoWeb6 from '../assets/web6.png';
+import img2 from '../assets/fondo1.jpg'
+
 
 export const LandingPage = () => {
   const [darkMode, setdarkMode] = useState(false)
@@ -23,8 +12,8 @@ export const LandingPage = () => {
 
       <main>
         <section>
-          <nav class="barra1">
-            <h1 class="title1">TiendAnimal</h1>
+          <nav className="barra1">
+            <h1 className="title1">TiendAnimal</h1>
             <ul>
               <li>Conocenos</li>
               <li>Planes alimenticos</li>
@@ -32,7 +21,7 @@ export const LandingPage = () => {
               <li>
                 <img
                   onClick={() => setdarkMode(!darkMode)}
-                  className="cursor-pointer"
+                  className="img1"
                   src={logoDarkMode}
                   alt="logo"
                 />
@@ -41,12 +30,28 @@ export const LandingPage = () => {
             </ul>
           </nav>
         </section>
+        <section className='section2'>
+          <div className='divContainer'>
+            <div className='texto1'>
+              <h2>
+                ¿Que es el bienestar animal? <br/> <br/>
+              </h2>
+              <p>
+                En la actualidad el bienestar animal de nuestras mascotas, se trata de una cuestión que ha adquirido una enorme importancia en nuestra sociedad, no se trata unicamente de
+                garantizar que un animal esté libre de sufrimiento, sino también de asegurar que viva en un entorno que favorezca su desarrollo saludable, mental y social. Esto no solo incluye
+                el cuidado físico, como la alimentación adecuada y la atención médica, sino también aspectos emocionales y psicológicas.
+                <br/>
+                Las mascotas al ser animales sociales y dependientes de los humanos, requieren de atencion especial para asegurarse de que su entorno, sus interacciones y su día a día favorezcan
+                su bienestar general.
+                
+              </p>
+            </div>
 
-        <section>
-          <p>Esta apgina esta dedicada a integrar un concepto de como los dueños <br/>
-          deben de relacionar la salud de sus mascotas para la mejora del bienestar de <b/>
-          las mismas.
-          </p>
+            <div className='img2'>
+              <img src={img2} alt="" className='img2-1' />
+            </div>
+
+          </div>
         </section>
       </main>
     </div>
