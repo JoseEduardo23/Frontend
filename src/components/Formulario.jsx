7a518d7df9cc5d25b78b484aae449cc2f9ab86a3
Eduardo <joselincango73@gmail.com>
2025-02-19 22:19:53 -0500

    Configuracion de validaciones de los campos de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -14,65 +14,123 @@ export const Formulario = ({ producto }) => {
         categoria: producto?.categoria ?? "",
     });
     const [error, setError] = useState(null);
+    const [nombreError, setNombreError] = useState(null);
+    const [descripcionError, setDescripcionError] = useState(null);
+    const [precioError, setPrecioError] = useState(null);
+    const [stockError, setStockError] = useState(null);
+    const [categoriaError, setCategoriaError] = useState(null)
+
 
     const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value,
-        });
+        const { name, value } = e.target;
+
+        if (name === "nombre") {
+            if (!/^[A-Za-z\s]*$/.test(value)) {
+                setNombreError("El nombre no puede contener números ni caracteres especiales.");
+            }else if (value.trim() === "") {
+                setNombreError("Campo vacío.");
+            } else {
+                setNombreError(null);
+            }
+        }
+
+        if (name === "descripcion") {
+            const charCount = value.length;
+            if (charCount > 70) {
+                setDescripcionError("La descripción no puede tener más de 50 letras.");
+            }else if (value.trim() === "") {
+                setDescripcionError("Campo vacío.");
+            } else {
+                setDescripcionError(null);
+            }
+        }
+
+        if (name === "precio") {
+            const precio = parseFloat(value);
+            if (isNaN(precio) || precio <= 0 || precio > 100) {
+                setPrecioError("Ingrese un valor válido (entre 0 y 100).");
+            } else {
+                setPrecioError(null);
+            }
+        }
+
+        if (name === "stock") {
+            const stock = parseFloat(value)
+            if (isNaN(stock) || stock <= 0 || stock > 400) {
+                setStockError("Ingrese un valor válido")
+            } else {
+                setStockError(null)
+            }
+        }
+
+        if (name === "categoria") {
+            const charCount = value.length;
+            if (charCount > 15) {
+                setCategoriaError("La categoria no puede tener más de 15 letras.");
+                return;
+            }
+            else if (/\d/.test(value)) {
+                setCategoriaError("La categoría no puede contener números.");
+            } else if (value.trim() === "") {
+                setCategoriaError("Ingrese una categoría válida.");
+            } else {
+                setCategoriaError(null);
+            }
+        }
+
+
+
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: value,
+        }));
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setError(null); 
+        setError(null);
 
-        if (Object.values(form).includes("")) {
-            setError("Todos los campos son obligatorios");
+        if (Object.values(form).includes("") || nombreError || descripcionError || precioError || stockError || categoriaError) {
+            setError("Por favor, corrige los errores antes de enviar el formulario.");
             return;
         }
 
         try {
-            const token = localStorage.getItem('token');
+            const token = localStorage.getItem("token");
             if (!token) {
                 setError("No se encontró el token dentro del almacenamiento local");
                 return;
             }
 
             const headers = {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
                 Authorization: `Bearer ${token}`,
             };
 
             if (producto?._id) {
-                // Actualizar producto
                 const url = `${import.meta.env.VITE_BACKEND_URL}api/actualizar/producto/${producto._id}`;
                 await axios.put(url, form, { headers });
-                console.log("Producto actualizado correctamente");
-                toast.success("Producto actualizado correctamente")
+                toast.success("Producto actualizado correctamente");
             } else {
-                // Crear producto
                 const url = `${import.meta.env.VITE_BACKEND_URL}api/crear/producto`;
                 await axios.post(url, form, { headers });
-                console.log("Producto creado correctamente");
-                toast.success("Producto creado correctamente")
+                toast.success("Producto creado correctamente");
             }
 
             navigate("/dashboard/listar");
         } catch (error) {
             if (error.response && error.response.data) {
-                setError(error.response.data.msg); 
-                console.error("Error del servidor: ", error.response.data.msg);
-                toast.error(errorMessage);
+                setError(error.response.data.msg);
+                toast.error(error.response.data.msg);
             } else {
                 setError("Error inesperado: " + error.message);
-                console.error("Error inesperado:", error.message);
             }
         }
     };
 
     return (
         <form onSubmit={handleSubmit} className="formulario">
-            <ToastContainer/>
+            <ToastContainer />
             {error && <div className="error-message">{error}</div>}
             <div className="form-group">
                 <label className="form-label">Nombre del producto:</label>
@@ -85,6 +143,7 @@ export const Formulario = ({ producto }) => {
                     onChange={handleChange}
                     value={form.nombre || ""}
                 />
+                {nombreError && <p style={{ color: "red", fontSize: "14px" }}>{nombreError}</p>}
             </div>
 
             <div className="form-group">
@@ -97,6 +156,7 @@ export const Formulario = ({ producto }) => {
                     onChange={handleChange}
                     value={form.descripcion || ""}
                 />
+                {descripcionError && <p style={{ color: "red", fontSize: "14px" }}>{descripcionError}</p>}
             </div>
 
             <div className="form-group">
@@ -110,6 +170,7 @@ export const Formulario = ({ producto }) => {
                     onChange={handleChange}
                     value={form.precio || ""}
                 />
+                {precioError && <p style={{ color: "red", fontSize: "14" }}>{precioError}</p>}
             </div>
 
             <div className="form-group">
@@ -122,6 +183,7 @@ export const Formulario = ({ producto }) => {
                     onChange={handleChange}
                     value={form.stock || ""}
                 />
+                {stockError && <p style={{ color: "red", fontSize: "14" }}>{stockError}</p>}
             </div>
 
             <div className="form-group">
@@ -134,9 +196,11 @@ export const Formulario = ({ producto }) => {
                     onChange={handleChange}
                     value={form.categoria || ""}
                 />
+                {categoriaError && <p style={{ color: "red", fontSize: "14" }}>{categoriaError}</p>}
+
             </div>
 
-            
+
 
             <input
                 className="form-submit"
