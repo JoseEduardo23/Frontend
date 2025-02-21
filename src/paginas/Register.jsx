e72c5afd4deca66c4b666c5ab71746e15845b6fa
Eduardo <joselincango73@gmail.com>
2025-02-20 22:58:00 -0500

    Configuracion de validaciones del registro

--- src/paginas/Register.jsx
+++ src/paginas/Register.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Mensaje from '../components/Alertas/Mensaje';
+import engranaje from '../assets/engranaje.png'
 import '../Estilos/Register.css'
 import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export const Register = () => {
 
@@ -17,17 +18,106 @@ export const Register = () => {
         password: ""
     });
 
+    /*Manejo de errores*/
+
+    const [error, setError] = useState(null);
+    const [nombreError, setNombreError] = useState(null);
+    const [apellidoError, setApellidoError] = useState(null);
+    const [direccionError, setDireccionError] = useState(null);
+    const [telefonoError, setTelefonoError] = useState(null);
+    const [passwordError, setPasswordError] = useState(null);
+
+
+
+
+    const [showPassword, setShowPassword] = useState(false)
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+
+
     const handleChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        });
+
+        const { name, value } = e.target;
+
+
+        if (name === "nombre") {
+
+            const charCount = value.length;
+            if (charCount > 10) {
+                setNombreError("Exceso de carácteres.");
+                return;
+            }
+
+            if (!/^[A-Za-z\s]*$/.test(value)) {
+                setNombreError("El nombre no debe tener números");
+            } else {
+                setNombreError(null);
+            }
+        }
+        if (name === "apellido") {
+
+            const charCount = value.length;
+            if (charCount > 10) {
+                setApellidoError("Exceso de carácteres.");
+                return;
+            }
+
+            if (!/^[A-Za-z\s]*$/.test(value)) {
+                setApellidoError("El apellido no debe tener números");
+            } else {
+                setApellidoError(null);
+            }
+        }
+        if (name === "direccion") {
+            const charCount = value.length;
+            if (charCount > 25) {
+                setDireccionError("Exceso de carácteres.");
+                return;
+            } else {
+                setDireccionError(null);
+            }
+        }
+
+        if (name === "telefono") {
+            const numberCount = value.length;
+            if (numberCount > 15) {
+                setTelefonoError("Exceso de numeros")
+                return;
+            } else if (/[^0-9]/.test(value)) {
+                setTelefonoError("Solo debe contener números.");
+            }
+
+        }
+
+        if (name === "password") {
+         if (!/(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}/.test(value)) {
+                setPasswordError(<p>Debe contener una mayúscula, <br /> 
+                                  un número y un carácter especial</p>);
+            } else {
+                setPasswordError(null);
+            }
+        }
+
+
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: value,
+        }));
     };
 
     const [mensaje, setMensaje] = useState("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setError(null);
+
+        if (Object.values(form).includes("") || nombreError || apellidoError || direccionError || telefonoError) {
+            setError("Por favor, corrige los errores antes de enviar el formulario de registro.");
+            return;
+        }
+
+
         try {
             const url = `${import.meta.env.VITE_BACKEND_URL}api/register`;
             const respuesta = await axios.post(url, form);
@@ -42,18 +132,22 @@ export const Register = () => {
     return (
         <>
             <div className="container">
+                <div className='engra'>
+                    <img src={engranaje} alt="" className='engr' />
+                    <p>
+                        Registrate es gratis!
+                    </p>
+                </div>
                 {Object.keys(mensaje).length > 0 && (
                     <div className={`message ${mensaje.tipo}`}>
                         {mensaje.respuesta}
                     </div>
                 )}
                 <div className="formcontainer">
-
                     <h1 className="title">BIENVENIDO</h1>
-                    <small className="subtitle">Ingresa tus datos</small>
 
-                    <form className='register-form' onSubmit={handleSubmit}>
-
+                    <form  className='register-form' onSubmit={handleSubmit}>
+                        {error && <div style={{ color: "red", fontSize: "10px" }} className='error-message'>{error}</div>}
                         <div className="mb-3">
                             <label className="mb-2 block text-sm font-semibold" htmlFor="nombre">Nombre:</label>
                             <input
@@ -64,11 +158,12 @@ export const Register = () => {
                                 onChange={handleChange}
                                 placeholder="Ingresa tu nombre"
                                 className="input"
-                                
                             />
+                            {nombreError && <p className='error-m' style={{ color: "red", fontSize: "12px" }}>{nombreError}</p>}
+
                         </div>
 
-                        <div className="mb-3">
+                        <div className="inp">
                             <label className="mb-2 block text-sm font-semibold" htmlFor="apellido">Apellido:</label>
                             <input
                                 type="text"
@@ -78,8 +173,10 @@ export const Register = () => {
                                 onChange={handleChange}
                                 placeholder="Ingresa tu apellido"
                                 className="input"
-                                
+
                             />
+                            {apellidoError && <p className='error-m' style={{ color: "red", fontSize: "12px" }}>{apellidoError}</p>}
+
                         </div>
 
                         <div className="mb-3">
@@ -92,11 +189,13 @@ export const Register = () => {
                                 onChange={handleChange}
                                 placeholder="Ingresa tu dirección"
                                 className="input"
-                                
+
                             />
+                            {direccionError && <p className='error-m' style={{ color: "red", fontSize: "12px" }}>{direccionError}</p>}
+ 
                         </div>
 
-                        <div className="mb-3">
+                        <div className="inp">
                             <label className="mb-2 block text-sm font-semibold" htmlFor="telefono">Teléfono:</label>
                             <input
                                 type="tel"
@@ -106,39 +205,47 @@ export const Register = () => {
                                 onChange={handleChange}
                                 placeholder="Ingresa tu teléfono"
                                 className="input"
-                                
+
                             />
+                            {telefonoError && <p className='error-m' style={{ color: "red", fontSize: "12px" }}>{telefonoError}</p>}
+
                         </div>
 
                         <div className="mb-3">
-                            <label className="mb-2 block text-sm font-semibold" htmlFor="email">Email:</label>
+                            <label className="mb-2 block text-sm font-semibold" htmlFor="email">Correo electrónico:</label>
                             <input
                                 type="email"
                                 id="email"
                                 name="email"
                                 value={form.email || ""}
                                 onChange={handleChange}
-                                placeholder="Ingresa tu email"
+                                placeholder="Ingresa tu correo"
                                 className="input"
-                                
+
                             />
                         </div>
 
-                        <div className="mb-3">
+                        <div className="inp">
                             <label className="mb-2 block text-sm font-semibold" htmlFor="password">Contraseña:</label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                value={form.password || ""}
-                                onChange={handleChange}
-                                className="input"
-                                
-                            />
+                            <div className="password-container">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    name="password"
+                                    value={form.password || ""}
+                                    onChange={handleChange}
+                                    placeholder='Ingrese su contraseña'
+                                    className="input"
+                                />
+                                <span className="toggle-password" onClick={togglePasswordVisibility}>
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </span>
+                            </div>
+                            {passwordError && <p className='error-m' style={{ color: "red", fontSize: "12px" }}>{passwordError}</p>}
                         </div>
 
-                        <div className="mb-3">
-                            <button className="button">Registrar</button>
+                        <div className="butt-cont">
+                            <button className="reg-button">Registrar</button>
                         </div>
 
                     </form>
@@ -147,8 +254,8 @@ export const Register = () => {
                         <p>¿Ya tienes una cuenta?</p>
                         <Link to="/login" className="link">Iniciar sesión</Link>
                     </div>
-
                 </div>
+
             </div>
             <ToastContainer position="top-right" autoClose={5000} hideProgressBar={true} />
         </>
