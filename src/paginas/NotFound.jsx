16dfac0894432ef6e0b2a0389a5186f78bde6e67
Eduardo <joselincango73@gmail.com>
2025-03-16 20:11:04 -0500

    Rediseño, Not Found

--- src/paginas/NotFound.jsx
+++ src/paginas/NotFound.jsx
@@ -1,14 +1,18 @@
-import logoDog from '../assets/error.png';
+import logoDog from '../assets/404.png';
 import { Link } from 'react-router-dom';
-import '../Estilos/NotFound.css'
+import '../Estilos/NotFound.css';
+import VID from '../assets/fondos/nature.gif';
+
 export const NotFound = () => {
     return (
         <div className="notfound-container">
+            <p className="notfound-title">Página no encontrada</p>
+
+            <img src={VID} id="video-background" />
             <img className="notfound-logo" src={logoDog} alt="Página no encontrada" />
             <div className="notfound-text-container">
-                <p className="notfound-title">Página no encontrada</p>
-                <p className="notfound-message">Lo sentimos, la página que has solicitado no se ha encontrado.</p>
-                <Link to="/login" className="notfound-button">Ir al Login</Link>
+                <p className="notfound-message">La página que buscas fue movida, aliminada, reacomodada o puede que nunca haya existido... ¿quién sabe?</p>
+                <Link to="/" className="notfound-button">Página principal</Link>
             </div>
         </div>
     );
